834c234e977277d18c0b033919e67de9dd3bcdcc
Szymon Stepniak <szymon.stepniak@gmail.com>
2016-10-08 19:16:20 +0200

    spock framework for tests added

---
+++ src/test/java/com/github/wololock/RestDemoApplicationSpec.groovy
@@ -0,0 +1,9 @@
+package com.github.wololock
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class RestDemoApplicationSpec extends Specification {
+
+}
