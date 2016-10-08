b8d9a7b563d7bb5bc4224795086946b6dff3d333
Szymon Stepniak <szymon.stepniak@gmail.com>
2016-10-08 19:11:17 +0200

    initial import

---
+++ src/main/java/com/github/wololock/RestDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.wololock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoApplication.class, args);
+	}
+}
